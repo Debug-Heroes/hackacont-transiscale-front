b6ac00d248a7e4690c067b0571fd1285a14fa02c
Talisson Faria <tfxx.sf@gmail.com>
2024-11-23 12:21:31 -0300

    style: login page style adjustments

--- src/page/login.tsx
+++ src/page/login.tsx
@@ -1,7 +1,7 @@
 export function Login() {
     return (
         <div className="h-screen w-screen flex justify-center items-center ">
-            <div className="bg-gray-200 w-80 h-[22rem] rounded-sm p-5">
+            <div className="bg-gray-100 w-80 h-[22rem] rounded-sm p-5 shadow-xl ">
                 <h1 className="text-2xl font-semibold">Login</h1>
                 <form className="mt-5 flex flex-col gap-3">
                     <label htmlFor="" className="text-lg">Usuario</label>
