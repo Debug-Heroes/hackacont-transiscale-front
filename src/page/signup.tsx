f5bb2a2cdfd0fbe625b177f6ee82d77b3d9ac3dd
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-11-23 12:35:58 -0300

    feat:  add page signup

---
+++ src/page/signup.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input/Input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const RegisterComponiesSchema = z.object({
+  name: z.string(),
+  email: z.string().email().optional(),
+  address: z.string().optional(),
+  contact_number: z.number().optional(),
+  password: z.string().min(1),
+  passwordConfirm: z.string().min(1),
+})
+
+type IRegisterComponies = z.infer<typeof RegisterComponiesSchema>
+export function SignUp() {
+
+  const { register, handleSubmit } = useForm<IRegisterComponies>({
+    resolver: zodResolver(RegisterComponiesSchema),
+    defaultValues: {
+      name: '',
+      address: '',
+      contact_number: NaN,
+      email: '',
+      password: '',
+      passwordConfirm: ''
+    }
+  })
+
+  return (
+    <div className="flex justify-center items-center h-screen p-1">
+      <form action="" className="relative flex flex-col gap-4 w-full max-w-md shadow-xl bg-blue-950 text-gray-50  rounded-md  p-8">
+        <h2 className="text-xl font-semibold">Cadastrar empresa</h2>
+        <Input
+          label="Nome"
+          placeholder="Digite o seu nome"
+
+        />
+        <Input
+          label="Email"
+          type="email"
+          placeholder="Digite o seu email"
+        />
+        <Input
+          label="Endereço completo"
+
+          placeholder="Digite o seu endereço"
+        />
+        <Input
+          label="Contato"
+          type="number"
+          placeholder="Digite seu numero para contato"
+        />
+
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
+    </div>
+  )
+}
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,5 +1,6 @@
 import { Home } from '@/page/home';
 import { Login } from '@/page/login'
+import { SignUp } from '@/page/signup';
 import { createBrowserRouter } from 'react-router-dom';
 
 
@@ -11,5 +12,9 @@ export const router = createBrowserRouter([
   {
     path: '/signin',
     element: <Login />
+  },
+  {
+    path: '/sign-up',
+    element: <SignUp />
   }
 ])
