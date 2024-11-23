b28d6dfcd24f3418d71c2ddf29e417f91f5ab56e
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-11-23 14:42:47 -0300

    chore: configure provider toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Router } from "./router/router-provider";
 import { queryClient } from "./lib/react-query";
-import { AuthVerification } from "./contexts/AuthVerification";
+import { Toaster } from 'sonner'
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-
+      <Toaster />
       <Router />
-
     </QueryClientProvider>
   )
 }
