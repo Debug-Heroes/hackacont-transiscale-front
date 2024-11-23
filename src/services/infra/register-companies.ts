a7ba18a8fec89cf119675a09ca9d6e8710cdf120
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-11-23 14:50:44 -0300

    chore: configure file for routes API

---
+++ src/services/infra/register-companies.ts
@@ -0,0 +1,33 @@
+import { api } from "@/lib/axios"
+
+interface RegisterCompaniesRequest {
+  name: string
+  email: string
+  address?: string
+  contact_number: string
+  password: string
+  confirmPassword: string
+
+
+}
+
+interface RegisterCompaniesResponse {
+  id: string
+  name: string
+  address: string
+  contact_number?: string
+
+}
+export async function registerCompanies(data: RegisterCompaniesRequest){
+  const { name, email, address, contact_number, password, confirmPassword} = data 
+  const response = await api.post<RegisterCompaniesResponse>('/signup', { 
+    name, 
+    email, 
+    address, 
+    contact_number, 
+    password, 
+    confirmPassword
+  })
+
+  return response.data
+}
