deb1113dc46a2a9dbbfcec43137ebc127a57279d
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-11-23 23:49:56 -0300

    feat: add API request for login and error handling on login page layout

---
+++ src/services/infra/authenticate.ts
@@ -0,0 +1,20 @@
+import { api } from "@/lib/axios"
+
+interface AuthenticateRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateResponse {
+  accessToken: string
+
+}
+export async function authenticate(data: AuthenticateRequest){
+  const { email, password} = data 
+  const response = await api.post<AuthenticateResponse>('/login', { 
+    email, 
+    password, 
+  })
+
+  return response.data
+}
