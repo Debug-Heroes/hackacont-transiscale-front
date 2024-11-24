2bd0042319e7a7b7e3a9188b65fb4d5d042761b3
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-11-23 23:50:11 -0300

    feat: add spinner component

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,6 @@
+
+export function Spinner() {
+  return (
+    <div className="border mx-auto border-transparent rounded-full size-6 text-center border-t-cyan-50 border-r-cyan-50 animate-spin" />
+  )
+}
