1708def101ec9ecf7c133a7ae3f304d1665f5932
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-11-24 01:49:02 -0300

    feat: register employees with api rest

--- src/components/RegisterEmployees/index.tsx
+++ src/components/RegisterEmployees/index.tsx
@@ -6,31 +6,47 @@ import { z } from 'zod'
 import { Controller, useForm } from 'react-hook-form'
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "../Input/Input";
+import { toast } from "sonner";
 
 const EmployeesSchema = z.object({
-  name: z.string()
+  name: z.string().min(1),
+  role: z.string().min(1),
+  current_weekly_hours: z.coerce.number(),
+  current_working_days: z.coerce.number(),
+  weekly_productivity: z.coerce.number()
 })
 
 type EmployeesSchemaType = z.infer<typeof EmployeesSchema>
 export function RegisterEmployees() {
-  const { handleSubmit, control } = useForm<EmployeesSchemaType>({
+  const { handleSubmit, control, reset } = useForm<EmployeesSchemaType>({
     resolver: zodResolver(EmployeesSchema),
     defaultValues: {
-      name: ''
+      name: '',
+      role: '',
+      current_weekly_hours: NaN,
+      current_working_days: NaN,
+      weekly_productivity: NaN
     }
   })
 
   const { mutate } = useMutation({
     mutationFn: (data: EmployeesSchemaType) => registerEmployees({
-      name: data.name
-    })
+      ...data
+    }),
+    onSuccess(_, variables, ___) {
+      toast.success(`Funcionário ${variables.name} foi criado`)
+      reset()
+    },
+    onError(_, __, ___) {
+      toast.error(`Não foi possível cadastrar um funcionário. Tente novamente`)
+    },
   })
 
   function handleRegisterEmployees(data: EmployeesSchemaType) {
-    const { name } = data
+    const { name, current_weekly_hours, current_working_days, role, weekly_productivity } = data
 
     mutate({
-      name
+      name, current_weekly_hours, current_working_days, role, weekly_productivity
     })
   }
   return (
@@ -57,35 +73,73 @@ export function RegisterEmployees() {
               render={({ field }) => {
                 return (
                   <Input
-                    label="Name"
-                    placeholder="Enter the name"
+                    label="Nome"
+                    placeholder="Inseri o nome do funcionário"
                     {...field}
                   />
                 )
               }}
 
             />
-            <Input
-              label="Prod dia 6x1"
-              placeholder="Prod dia 6x1"
+            <Controller
+              name="role"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <Input
+                    label="Cargo"
+                    placeholder="Inseri o nome do cargo"
+                    {...field}
+                  />
+                )
+              }}
+
             />
-            <Input
-              label="Prod dia 4x3"
-              placeholder="Prod dia 4x3"
-              type="number"
-              min={1}
+            <Controller
+              name="current_weekly_hours"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <Input
+                    label="Horas semanais atuais"
+                    type="number"
+                    placeholder="Inseri as horas semanais atuais"
+                    {...field}
+                  />
+                )
+              }}
+
             />
-            <Input
-              label="horas_trabalhadas"
-              placeholder="horas_trabalhadas"
-              type="number"
-              min={1}
+            <Controller
+              name="current_working_days"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <Input
+                    label="Dias de trabalho atuais"
+                    type="number"
+                    placeholder="Inseri os dias de trabalho atuais"
+                    {...field}
+                  />
+                )
+              }}
+
             />
-            <Input
-              label="escala_atual"
-              placeholder="escala_atual"
-              type="number"
-              min={1}
+            <Controller
+              name="weekly_productivity"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <Input
+                    label="Produtividade semanal"
+                    type="number"
+                    step=".01"
+                    placeholder="Inseri a produtividade semanal"
+                    {...field}
+                  />
+                )
+              }}
+
             />
             <Button className="mt-4">
               Registrar
--- src/services/infra/register-employees.ts
+++ src/services/infra/register-employees.ts
@@ -1,15 +1,26 @@
 import { api } from "@/lib/axios"
+import { getIdCompanies } from "@/utils/get-id-companies"
 
 interface RegisterEmployeesRequest {
   name: string
+  role: string
+  current_weekly_hours: number
+  current_working_days : number
+  weekly_productivity : number
 }
 
 interface RegisterEmployeesResponse {
 
 }
-export async function registerEmployees({name}: RegisterEmployeesRequest){
-  const response = await api.post<RegisterEmployeesResponse>('/empo', {
-  name
+export async function registerEmployees({name, role, current_weekly_hours, current_working_days, weekly_productivity}: RegisterEmployeesRequest){
+  const response = await api.post<RegisterEmployeesResponse>('/employee', {
+  name,
+  role, 
+  current_weekly_hours, 
+  current_working_days, 
+  weekly_productivity: (weekly_productivity).toFixed(2),
+  company_id: getIdCompanies()
+
   })
 
   return response.data
