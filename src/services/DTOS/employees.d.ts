5c773eb680b28554798f883a7eaa3033691db168
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-11-24 05:10:15 -0300

    feat: add DTO typing for employees

---
+++ src/services/DTOS/employees.d.ts
@@ -0,0 +1,11 @@
+export interface Employee {
+  id: string
+  company_id: string
+  name: string
+  role: string
+  current_weekly_hours: number
+  current_working_days: number
+  weekly_productivity: string
+  created_at: string
+  updated_at: string
+}
