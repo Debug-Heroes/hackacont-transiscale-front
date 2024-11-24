355bfcd3bd18ebf86bd627200cec64b32bfbba0f
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2024-11-24 05:10:15 -0300

    feat: add DTO typing for employees

--- src/services/infra/get-all-employees.ts
+++ src/services/infra/get-all-employees.ts
@@ -1,19 +1,12 @@
 import { api } from "@/lib/axios"
+import { Employee } from "../DTOS/employees"
 
 interface GetAllEmployeesRequest {
  
 }
 
-interface GetAllEmployeesResponse {
-  id: string
-  company_id: string
-  name: string
-  role: string
-  current_weekly_hours: number
-  current_working_days: number
-  weekly_productivity: string
-  created_at: string
-  updated_at: string
+interface GetAllEmployeesResponse extends Employee{
+
 }
 export async function getAllEmployees({}: GetAllEmployeesRequest){
   const response = await api.get<GetAllEmployeesResponse[]>(`/employee`, {
